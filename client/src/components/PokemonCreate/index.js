2d163b6df3a7901768eae624803c70b318dc7a11
LauzarateARG <lautarozarate212001@gmail.com>
2021-11-11 09:24:06 -0300

    bugs fix

--- client/src/components/PokemonCreate/index.js
+++ client/src/components/PokemonCreate/index.js
@@ -2,33 +2,45 @@ import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { postPokemon, getTypes } from "../../actions/index";
 import { useDispatch, useSelector } from "react-redux";
+import style from "./style.module.css";
 
 function validate(input) {
-  let errors = {};
+  let errors = {
+    name:""
+  };
   if (!input.name) {
     errors.name = "debe ingresar un nombre";
-  } else if (
-    input.hp > 255 ||
-    input.attack > 255 ||
-    input.defense > 255 ||
-    input.speed > 255
-  ) {
-    errors.name = "pokemon muy poderoso, sus stats no pueden superar 255";
-  } else if (input.hp < 0 || input.hp < 0 || input.hp < 0 || input.hp < 0) {
-    errors.name = "pokemon muy debil, sus stats no pueden ser menor a 0";
-  } else if (input.height > 20) {
-    errors.name = "el pokemon es demasiado alto, altura maxima 20m";
-  } else if (input.height <= 0) {
-    errors.name =
-      "el pokemon es demasiado bajo, su altura tiene que ser mayor a 0";
-  } else if (input.weight > 100) {
-    errors.name = "el pokemon tiene mucho peso, peso maximo 100kg";
-  } else if (input.weight <= 0) {
-    errors.name = "el pokemon tiene poco peso, su peso tiene que ser mayor a 0";
-  } else if (input.type.length > 2) {
-    errors.name = "solo se permiten 2 tipos por pokemon";
-  } else if (input.type.length === 0) {
-    errors.name = "el pokemon debe tener tipo";
+    if (
+      input.hp > 255 ||
+      input.attack > 255 ||
+      input.defense > 255 ||
+      input.speed > 255
+    ) {
+      errors.name = "pokemon muy poderoso, sus stats no pueden superar 255";
+    }
+    else if (input.hp < 0 || input.hp < 0 || input.hp < 0 || input.hp < 0) {
+      errors.name = "pokemon muy debil, sus stats no pueden ser menor a 0";
+    }
+    else if (input.height > 200) {
+      errors.name = "el pokemon es demasiado alto, altura maxima 200mm";
+    }
+    else if (input.height <= 0) {
+      errors.name =
+        "el pokemon es demasiado bajo, su altura tiene que ser mayor a 0";
+    }
+    else if (input.weight > 100) {
+      errors.name = "el pokemon tiene mucho peso, peso maximo 100kg";
+    }
+    else if (input.weight <= 0) {
+      errors.name =
+        "el pokemon tiene poco peso, su peso tiene que ser mayor a 0";
+    }
+    else if (input.type.length > 2) {
+      errors.name = "solo se permiten 2 tipos por pokemon";
+    }
+    else if (input.type.length === 0) {
+      errors.name = "el pokemon debe tener tipo";
+    }
   }
 }
 
@@ -57,10 +69,7 @@ export default function PokemonCreate() {
       [e.target.name]: e.target.value,
     });
     setErrors(
-      validate({
-        ...input,
-        [e.target.name]: e.target.value,
-      })
+      validate({input})
     );
     console.log(input);
   }
@@ -70,6 +79,7 @@ export default function PokemonCreate() {
       ...input,
       type: [...input.type, e.target.value],
     });
+    setErrors(input);
     console.log(input);
   }
 
@@ -103,13 +113,10 @@ export default function PokemonCreate() {
 
   return (
     <div>
-      <Link to="/home">
-        <button>volver</button>
-      </Link>
       <h1> crea tu propio Pokemon </h1>
 
       <form onSubmit={(e) => handlerSubmit(e)}>
-        <div>
+        <div className={style.form}>
           <label> Nombre </label>
           <input type="text" name="name" onChange={handleChange} />
 
@@ -127,16 +134,22 @@ export default function PokemonCreate() {
           <input type="number" name="height" onChange={handleChange} />
 
           <label> tipo </label>
-
-          <select onChange={(e) => handlerChange(e)}>
-            {type?.map((el) => {
-              return <option value={el.name}> {el.name} </option>;
-            })}
-          </select>
-          <button onClick={handlerDelet}> restablecer tipos </button>
+          <div>
+            <select onChange={(e) => handlerChange(e)}>
+              {type?.map((el) => {
+                return <option value={el.name}> {el.name} </option>;
+              })}
+            </select>
+            <button onClick={handlerDelet}> restablecer tipos </button>
+          </div>
 
           <h4>{input.type.map((el) => el + " ,")}</h4>
-          <button type="submit"> Crear </button>
+
+          {errors ? (
+            <p>{errors.name}</p>
+          ) : (
+            <button type="submit"> Crear </button>
+          )}
         </div>
       </form>
     </div>
